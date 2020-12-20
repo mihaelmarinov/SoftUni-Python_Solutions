5694564f6af11a37f93a91f4e8078331d7e31ab6
mihaelmarinov <71415032+mihaelmarinov@users.noreply.github.com>
2020-12-20 13:29:02 -0800

    Create 03. List Statistics.py

---
+++ Python_ProgramingFundamentals/Lists Basics - Lab/03. List Statistics.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+positive = []
+negative = []
+
+for i in range(n):
+    num = int(input())
+
+    if num >= 0:
+        positive.append(num)
+    else:
+        negative.append(num)
+
+print(positive)
+print(negative)
+print(f"Count of positives: {len(positive)}. Sum of negatives: {sum(negative)}")
